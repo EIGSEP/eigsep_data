2023ca726cc510d566a77a3b58c269afae7f2bbb
Christian Hellum Bye <chbye@berkeley.edu>
2025-07-28 16:34:56 -0700

    separate calibration from data handling

--- src/eigsep_data/imu.py
+++ src/eigsep_data/imu.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
@@ -59,54 +60,22 @@ def _get_skew_symmetric_matrix(v):
     return np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
 
 
-class Imu:
+@dataclass(frozen=True)
+class ImuCalibrator:
     """
-    Class for handling IMU data. Input data are raw readings from the
-    IMU (see EIGSEP/pico-firmware for the expected format). The class
-    allows defining the coordinate system and provides methods to get
-    orientation in this coordinate system based on different IMU sensor
-    readings (gravity, magnetic field, etc.).
+    Class for calibrating IMU data. It contains the rotation matrix
+    that transforms the IMU data from the native coordinate system to
+    the desired coordinate system.
+
     """
+    Rmat: np.ndarray
 
-    def __init__(self, imu_data):
+    @classmethod
+    def from_ref(cls, grav_ref, mag_ref=None):
         """
-        Parameters
-        ----------
-        imu_data : dict
-            See EIGSEP/pico-firmware for the expected format.
-
-        """
-        self.data = imu_data
-        # rotation matrix for coordinate system conversion
-        self._coord_Rmat = np.eye(3)
-
-    @property
-    def coord_Rmat(self):
-        """Return the rotation matrix for coordinate system conversion."""
-        return self._coord_Rmat
-
-    @coord_Rmat.setter
-    def coord_Rmat(self, value):
-        """
-        Set the rotation matrix for coordinate system conversion.
-
-        Parameters
-        ----------
-        value : np.ndarray
-            A 3x3 rotation matrix.
-
-        """
-        if isinstance(value, np.ndarray) and value.shape == (3, 3):
-            self._coord_Rmat = value
-        else:
-            raise ValueError("Rotation matrix must be a 3x3 numpy array.")
-
-    def rot_vector(self, v):
-        return self.coord_Rmat @ v
-
-    def set_reference(self, grav_ref, mag_ref=None):
-        """
-        Set the reference vectors for the coordinate system.
+        Create an ImuCalibrator instance from the reference vectors
+        specifying the direction of the gravity vector and the
+        magnetic field vector in the desired coordinate system.
 
         Parameters
         ----------
@@ -115,6 +84,10 @@ class Imu:
         mag_ref : np.ndarray
             The direction of the magnetic field vector (x, north) in the
             coordinate system.
+
+        Returns
+        -------
+        ImuCalibrator
 
         """
         # get the matrix that rotates the gravity vector to the z-axis
@@ -129,39 +102,118 @@ class Imu:
         xp = xp - np.array([0, 0, xp[2]])
         xp /= np.linalg.norm(xp)
         R2 = get_rotation_matrix(xp, np.array([1, 0, 0]))
+        return cls(R2 @ R1)
 
-        self.coord_Rmat = R2 @ R1
+    @classmethod
+    def from_imu_data(cls, imu_data : dict):
+        """
+        Create an ImuCalibrator instance from an IMU reading at the
+        ``home`` position, i.e., the position where the IMU is aligned
+        with the desired coordinate system.
 
-    @property
-    def acceleration(self):
-        """Return the acceleration vector in m/s^2."""
-        v = np.array(
-            [self.data["accel_x"], self.data["accel_y"], self.data["accel_z"]]
-        )
-        return self.rot_vector(v)
+        Parameters
+        ----------
+        imu_data : dict
+            See EIGSEP/pico-firmware for the expected format.
 
-    @property
-    def linear_acceleration(self):
-        """Return the linear acceleration vector in m/s^2."""
-        v = np.array(
+        Returns
+        -------
+        ImuCalibrator
+
+        """ 
+        grav_ref = np.array(
             [
-                self.data["lin_accel_x"],
-                self.data["lin_accel_y"],
-                self.data["lin_accel_z"],
+                imu_data["accel_x"] - imu_data["lin_accel_x"],
+                imu_data["accel_y"] - imu_data["lin_accel_y"],
+                imu_data["accel_z"] - imu_data["lin_accel_z"],
             ]
         )
-        return self.rot_vector(v)
+        mag_ref = np.array(
+            [imu_data["mag_x"], imu_data["mag_y"], imu_data["mag_z"]]
+        )
+        return cls.from_ref(grav_ref, mag_ref)
+
+    def apply(self, vec: np.ndarray) -> np.ndarray:
+        """
+        Apply the rotation matrix to a vector.
+
+        Parameters
+        ----------
+        vec : np.ndarray
+            A 3-element vector in the native coordinate system.
+
+        Returns
+        -------
+        np.ndarray
+            The vector in the desired coordinate system.
+
+        """
+        return self.Rmat @ vec
+
+
+@dataclass
+class ImuSnapshot:
+    """
+    Class for handling IMU data. Input data are raw readings from the
+    IMU (see EIGSEP/pico-firmware for the expected format). The class
+    allows using ImuCalibrator to define the coordinate system and 
+    provides methods to get orientation in this coordinate system based
+    on different IMU sensor readings (gravity, magnetic field, etc.).
+    """
+
+    accel : np.ndarray
+    lin_accel : np.ndarray
+    quat : np.ndarray
+
+    @classmethod
+    def from_imu_data(cls, imu_data: dict):
+        """
+        Create an ImuSnapshot instance from IMU data.
+
+        Parameters
+        ----------
+        imu_data : dict
+            See EIGSEP/pico-firmware for the expected format.
+
+        Returns
+        -------
+        ImuSnapshot
+
+        """
+        return cls(
+            accel=np.array(
+                [imu_data["accel_x"], imu_data["accel_y"], imu_data["accel_z"]]
+            ),
+            lin_accel=np.array(
+                [
+                    imu_data["lin_accel_x"],
+                    imu_data["lin_accel_y"],
+                    imu_data["lin_accel_z"],
+                ]
+            ),
+            quat=np.array(
+                [
+                    imu_data["quat_real"],
+                    imu_data["quat_i"],
+                    imu_data["quat_j"],
+                    imu_data["quat_k"],
+                ]
+            ),
+        )
 
     @property
     def gravity(self):
         """Return the gravity vector in m/s^2."""
-        return self.acceleration - self.linear_acceleration
+        return self.accel - self.linear_accel
 
-    def get_tilt_from_gravity(self):
+    def get_tilt_from_gravity(self, calibrator: ImuCalibrator = None):
         g_norm = self.gravity / np.linalg.norm(self.gravity)
-        return np.arccos(-g_norm[2])  # angle with respect to z-axis
-
-    def get_tilt_from_quat(self):
+        if calibrator is not None:
+            g_norm = calibrator.apply(g_norm)
+        th = np.arccos(-g_norm[2])  # angle with respect to z-axis
+        return th
+        
+    def get_tilt_from_quat(self, calibrator: ImuCalibrator = None):
         q = np.array(
             [
                 self.data["quat_real"],
@@ -173,4 +225,7 @@ class Imu:
         Rmat = R.from_quat(q).as_matrix()
         z_axis = np.array([0, 0, 1])
         g_norm = Rmat @ z_axis
+        g_norm /= np.linalg.norm(g_norm)
+        if calibrator is not None:
+            g_norm = calibrator.apply(g_norm)
         return np.arccos(-g_norm[2])
